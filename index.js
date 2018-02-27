3bbaba52ac3cad4d5f31cee13b4a921807ddf6c9
Andrii Lazurenko <andrey.lazurenko@gmail.com>
2018-02-27 21:54:12 +0100

    Fix with empty argument

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ const { exec } = require('child_process');
 const { promisify } = require('util');
 const run = promisify(exec);
 
+const log = (...logs) => console.log(`🎨 sassy-play:`, ...logs);
+
 const folder = argv._[0];
 
 (async () => {
   try {
+    if (!folder) {
+      log(`please, specify a folder name`);
+      return;
+    }
     await run(`mkdir ${folder}`);
     await run(`cp -a ${__dirname}/assets/. ${process.cwd()}/${folder}`);
-    console.log('🎨 sassy-play: bootstrapping a project for you...');
+    log('bootstrapping a project for you...');
     const result = await run(`npm i --prefix ${folder}`);
-    console.log(`🎨 sassy-play: done! ${process.uptime()} s`);
+    log(`done! ${process.uptime()} s`);
   } catch (e) {
-    console.log(e.stderr)
+    log(e.stderr)
   }
 })();
